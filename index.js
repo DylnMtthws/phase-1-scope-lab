1d506183d4cb43a62ca3e32ef66153d73096c2eb
dylanmatthews <djmatthews93@gmail.com>
2023-09-15 09:04:23 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {//I don't understand why this didn't use var?
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'me'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'someone else'
+}
